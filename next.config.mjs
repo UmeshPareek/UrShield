08d15cbea8e854156d27035406e357cf9052840a
UmeshPareek <pareeku01@gmail.com>
2026-02-14 02:34:10 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    // This forces the build to finish even if there are tiny type warnings
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // This prevents linting errors from stopping your Porsche from launching
-    ignoreDuringBuilds: true,
-  },
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+  // Optimized for Giant assets
+  images: { unoptimized: true }
 };
 
 export default nextConfig;
